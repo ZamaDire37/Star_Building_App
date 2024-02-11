6e42a7e8858995b101dfc19c7c643b3875d7dbec
ZamaDire <106476895+ZamaDire37@users.noreply.github.com>
2024-02-11 16:47:29 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Star_Building_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter the number of rows for the star pattern: ");
+                int rows = int.Parse(Console.ReadLine());
+
+                if (rows <= 0)
+                {
+                    throw new ArgumentException("Number of rows must be a positive integer.");
+                    
+                }
+
+                StarPrinter starPrinter = new StarPrinter(rows);
+                starPrinter.PrintStars();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: Please enter a valid integer.");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StarPrinter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Star_Building_App
+{
+    internal class StarPrinter
+    {
+        private int numRows;
+
+        public StarPrinter(int numRows)
+        {
+            this.numRows = numRows;
+        }
+
+        public void PrintStars()
+        {
+            for (int i = 1; i <= numRows; i++)
+            {
+                string spaces = new string(' ', numRows - i);
+                string stars = new string('*', 2 * i - 1);
+                Console.WriteLine(spaces + stars);
+            }
+        }
+    }
+}
